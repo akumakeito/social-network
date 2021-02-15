bdf1e58b2fafce4c5540e9e60d27980cdb60f8ee
madness4love <lokatin21@gmail.com>
2021-02-15 12:55:24 +0300

    initial commit. Task 2.2.1

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val firstPost = Post(
+        0,
+        1,
+        120221,
+        "Hola, Sinora",
+        likes = Likes()
+    )
+
+    val secondPost = Post(
+        0,
+        2,
+        120221,
+        "Hello, madam",
+        likes = Likes()
+    )
+
+    val thirdPost = Post(
+        0,
+        3,
+        140221,
+        "Hi, meow",
+        likes = Likes()
+    )
+}
